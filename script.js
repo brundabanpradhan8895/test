3192a1e8e4bf70b95645d6808b86c0a77da0fd67
brundabanpradhan8895 <pradhansikun824@gmail.com>
2023-10-09 19:08:34 +0530

    Number changed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Hello");
 
 
 // Create an array of numbers
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 // Use the filter method to get only even numbers
 const evenNumbers = numbers.filter(number => number % 2 === 0);
@@ -12,7 +12,7 @@ const doubledNumbers = evenNumbers.map(number => number * 2);
 
 // Print the original array, even numbers, and doubled numbers
 console.log("Original Numbers: " + numbers.join(", "));
-console.log("Even Numbers: " + evenNumbers.join(", "));
+console.log("Even Numbers: " + evenNumbers.join(', '));
 console.log("Doubled Even Numbers: " + doubledNumbers.join(", "));
 
 
